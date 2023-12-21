1c233df609b33bd7dbd49a7ed4a33bec68b2073b
Emmote <emmote2@gmail.com>
2023-12-21 15:17:20 +0000

    Update.
    - Adding Wintershield event routes.

--- docs/index.md
+++ docs/index.md
@@ -1,67 +1,74 @@
 # Idle Champions Briv Scripting Routes
 
 {::nomarkdown}
-<span class="routes">
-	<span class="routesRow">
-		<span class="routesDesc">
-			<h3 id="input">Please input your Briv's slot 4 item level as well as its rarity and gilding.</h3>
+<div class="tabs">
+	<input onClick="setHash('gemTab')" type="radio" class="tabsRadio" name="routesTabs" id="gemTab" checked>
+	<label for="gemTab" class="tabsLabel">Gem Farming</label>
+	<div class="tabsContent">
+		<span class="routesRow">
+			<span class="routesDesc">
+				<h3 id="input">Please input your Briv's slot 4 item level as well as its rarity and gilding.</h3>
+			</span>
 		</span>
-	</span>
-	<span class="routesRow">
-		<span class="routesCol1">
-			<label for="ilvl">Item Level:</label>
+		<span class="routesRow">
+			<span class="routesCol1">
+				<label for="ilvl">Item Level:</label>
+			</span>
+			<span class="routesCol2">
+				<input type="number" name="ilvl" id="ilvl" value="3626">
+			</span>
+			<span class="routesCol3">
+				<label for="presets">Specific Jumps:</label>
+			</span>
+			<span class="routesCol4">
+				<select name="presets" id="presets">
+				<option value="1j">1j</option>
+				<option value="2j">2j</option>
+				<option value="3j">3j</option>
+				<option value="4j" selected>4j</option>
+				<option value="5j">5j</option>
+				<option value="6j">6j</option>
+				<option value="7j">7j</option>
+				<option value="8j">8j</option>
+				<option value="9j">9j</option>
+				<option value="11j">11j</option>
+				</select>
+			</span>
 		</span>
-		<span class="routesCol2">
-			<input type="number" name="ilvl" id="ilvl" value="3626">
+		<span class="routesRow">
+			<span class="routesCol1">
+				<label for="rarity">Rarity:</label>
+			</span>
+			<span class="routesCol2">
+				<select name="rarity" id="rarity">
+				<option value="common">Common</option>
+				<option value="uncommon">Uncommon</option>
+				<option value="rare">Rare</option>
+				<option value="epic" selected>Epic</option>
+				</select>
+			</span>
 		</span>
-		<span class="routesCol3">
-			<label for="presets">Specific Jumps:</label>
+		<span class="routesRow">
+			<span class="routesCol1">
+				<label for="gilding">Gilding:</label>
+			</span>
+			<span class="routesCol2">
+				<select name="gilding" id="gilding">
+				<option value="none">None</option>
+				<option value="shiny">Shiny</option>
+				<option value="golden" selected>Golden</option>
+				</select>
+			</span>
+			<span class="routesCol5" id="shinyNote" style="display:none">
+				Note: You can't get 100% on even numbered jumps with Shiny.
+			</span>
 		</span>
-		<span class="routesCol4">
-			<select name="presets" id="presets">
-			<option value="1j">1j</option>
-			<option value="2j">2j</option>
-			<option value="3j">3j</option>
-			<option value="4j" selected>4j</option>
-			<option value="5j">5j</option>
-			<option value="6j">6j</option>
-			<option value="7j">7j</option>
-			<option value="8j">8j</option>
-			<option value="9j">9j</option>
-			<option value="11j">11j</option>
-			</select>
+		<span class="routesWrapper" id="wrapper">
+			&nbsp;
 		</span>
-	</span>
-	<span class="routesRow">
-		<span class="routesCol1">
-			<label for="rarity">Rarity:</label>
-		</span>
-		<span class="routesCol2">
-			<select name="rarity" id="rarity">
-			<option value="common">Common</option>
-			<option value="uncommon">Uncommon</option>
-			<option value="rare">Rare</option>
-			<option value="epic" selected>Epic</option>
-			</select>
-		</span>
-	</span>
-	<span class="routesRow">
-		<span class="routesCol1">
-			<label for="gilding">Gilding:</label>
-		</span>
-		<span class="routesCol2">
-			<select name="gilding" id="gilding">
-			<option value="none">None</option>
-			<option value="shiny">Shiny</option>
-			<option value="golden" selected>Golden</option>
-			</select>
-		</span>
-		<span class="routesCol5" id="shinyNote" style="display:none">
-			Note: You can't get 100% on even numbered jumps with Shiny.
-		</span>
-	</span>
-	<span class="routesWrapper" id="wrapper">
-		&nbsp;
-	</span>
-</span>
+	</div>
+	<input onClick="setHash('eventTab')" type="radio" class="tabsRadio" name="routesTabs" id="eventTab">
+	<label for="eventTab" class="tabsLabel">Events</label>
+	<div class="tabsContent" id="eventsList">&nbsp;</div>
+</div>
 {:/nomarkdown}
--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,88 +1,194 @@
-const ilvlInput=document.getElementById("ilvl");
-const presetsInput=document.getElementById("presets");
-const rarityInput=document.getElementById("rarity");
-const gildingInput=document.getElementById("gilding");
-const shinyNote=document.getElementById("shinyNote");
+const ilvlInput=document.getElementById(`ilvl`);
+const presetsInput=document.getElementById(`presets`);
+const rarityInput=document.getElementById(`rarity`);
+const gildingInput=document.getElementById(`gilding`);
+const shinyNote=document.getElementById(`shinyNote`);
 const fsa=`<br />This requires <a href="https://github.com/imp444/IC_Addons/tree/main/IC_BrivGemFarm_BrivFeatSwap_Extra" target="_blank">ImpEGamer's BrivFeatSwap</a> addon.`;
 const fcsa=`<br/>This will be faster with mouse clicks enabled.`;
-const jump=" checked";
-const tt="Tall Tales is in the Witchlight campaign.";
-const ll="The Roots of Loomlurch is in the Witchlight campaign.";
-const cf="Cursed Farmer is in The Grand Tour of the Sword Coast campaign.";
-const rac="Resolve Amongst Chaos is in the Descent into Avernus campaign."
-const bbb="Don't forget to enable Vajra patron to benefit from the Brisk Benefactor Tier 3 Corellon local blessing."
+const jump=` checked`;
+const tt=`Tall Tales is in the Witchlight campaign.`;
+const ll=`The Roots of Loomlurch is in the Witchlight campaign.`;
+const cf=`Cursed Farmer is in The Grand Tour of the Sword Coast campaign.`;
+const rac=`Resolve Amongst Chaos is in the Descent into Avernus campaign.`
+const bbb=`Don't forget to enable Vajra patron to benefit from the Brisk Benefactor Tier 3 Corellon local blessing.`
+const json=[
+	{
+		id: 1,
+		name: "Highharvestide",
+		bitfields: []
+	},
+	{
+		id: 2,
+		name: "Liars' Night",
+		bitfields: []
+	},
+	{
+		id: 3,
+		name: "Feast of the Moon",
+		bitfields: []
+	},
+	{
+		id: 4,
+		name: "Simril",
+		bitfields: []
+	},
+	{
+		id: 5,
+		name: "Wintershield",
+		bitfields: [
+			{
+				name: "1j",
+				bitfield: 843325416243167n
+			},
+			{
+				name: "2j",
+				bitfield: 966951756085247n
+			},
+			{
+				name: "3j",
+				bitfield: 1125623954636799n
+			},
+			{
+				name: "4j",
+				bitfield: 1068016077999467n
+			},
+			{
+				name: "5j",
+				custom: "Use the 4j route with the Wasting Haste feat."
+			},
+			{
+				name: "6j",
+				bitfield: 1116861684840824n
+			},
+			{
+				name: "7j",
+				bitfield: 1121482463737855n
+			},
+			{
+				name: "8j",
+				bitfield: 1125887020841951n
+			},
+			{
+				name: "9j",
+				bitfield: 965761360567659n
+			},
+			{
+				name: "10j",
+				bitfield: 597582421159869n
+			},
+			{
+				name: "11j",
+				bitfield: 949667735009275n
+			}
+		]
+	},
+	{
+		id: 9,
+		name: "Midwinter",
+		bitfields: []
+	},
+	{
+		id: 10,
+		name: "Grand Revel",
+		bitfields: []
+	},
+	{
+		id: 12,
+		name: "Fleetswake",
+		bitfields: []
+	},
+	{
+		id: 16,
+		name: "Festival of Fools",
+		bitfields: []
+	},
+	{
+		id: 18,
+		name: "Greengrass",
+		bitfields: []
+	},
+	{
+		id: 23,
+		name: "The Running",
+		bitfields: []
+	},
+	{
+		id: 26,
+		name: "The Great Modron March",
+		bitfields: []
+	},
+	{
+		id: 30,
+		name: "Dragondown",
+		bitfields: []
+	},
+	{
+		id: 37,
+		name: "Founder's Day",
+		bitfields: []
+	},
+	{
+		id: 38,
+		name: "Midsummer",
+		bitfields: []
+	},
+	{
+		id: 39,
+		name: "Ahghairon's Day",
+		bitfields: []
+	},
+	{
+		id: 49,
+		name: "Brightswords",
+		bitfields: []
+	}
+];
+
 
 function init() {
-	ilvlInput.addEventListener("input", update);
-	presetsInput.addEventListener("change", preset);
-	rarityInput.addEventListener("change", update);
-	gildingInput.addEventListener("change", update);
+	populateEventsList();
+	ilvlInput.addEventListener(`input`, update);
+	presetsInput.addEventListener(`change`, preset);
+	rarityInput.addEventListener(`change`, update);
+	gildingInput.addEventListener(`change`, update);
+	window.addEventListener('hashchange',() =>{swapTab();});
+	swapTab();
 	update();
 }
 
 function preset() {
-	rarityInput.value="epic";
-	gildingInput.value="golden";
+	rarityInput.value=`epic`;
+	gildingInput.value=`golden`;
 	
-	var jumps=presetsInput.value;
-	switch (jumps) {
-		case "1j":
-			ilvlInput.value=126;
-			break;
-		case "2j":
-			ilvlInput.value=626;
-			break;
-		case "3j":
-			ilvlInput.value=1626;
-			break;
-		case "5j":
-			ilvlInput.value=7626;
-			break;
-		case "6j":
-			ilvlInput.value=15626;
-			break;
-		case "7j":
-			ilvlInput.value=31626;
-			break;
-		case "8j":
-			ilvlInput.value=63626;
-			break;
-		case "9j":
-			ilvlInput.value=127626;
-			break;
-		case "11j":
-			ilvlInput.value=511626;
-			break;
-		default:
-			ilvlInput.value=3626;
-	}
+	var jumps=presetsInput.value.replace(/[^\d]/g, '');
+	ilvlInput.value=500*Math.pow(2,jumps-1)-374;
 	update();
 }
 
 function update() {
-	if (rarityInput.value!="epic") {
-		if (gildingInput.value=="golden") {
-			gildingInput.value="shiny";
+	if (rarityInput.value!=`epic`) {
+		if (gildingInput.value==`golden`) {
+			gildingInput.value=`shiny`;
 		}
 		if (gildingInput.options.length==3) {
 			gildingInput.options.remove(2);
 		}
 	} else {
 		if (gildingInput.options.length==2) {
-			var opt=document.createElement("option");
-			opt.text="Golden";
-			opt.value="golden";
+			var opt=document.createElement(`option`);
+			opt.text=`Golden`;
+			opt.value=`golden`;
 			gildingInput.options.add(opt);
 		}
 	}
-	if (gildingInput.value=="shiny"&&shinyNote.style.display=='none') {
+	if (gildingInput.value==`shiny`&&shinyNote.style.display=='none') {
 		shinyNote.style.display='';
-	} else if (gildingInput.value!="shiny"&&shinyNote.style.display=='') {
+	} else if (gildingInput.value!=`shiny`&&shinyNote.style.display=='') {
 		shinyNote.style.display='none';
 	}
 	var ilvls=ilvlInput.value;
 	if (ilvls<1) {
-		document.getElementById("ilvl").value=1;
+		document.getElementById(`ilvl`).value=1;
 		ilvls=1;
 	}
 	var rarity=determineRarity();
@@ -180,113 +286,111 @@ function unknownRoute() {
 
 function cursedFarmer() {
 	var bf = 1125899906842623n;
-	return addToDescRow(`<h3>Cursed Farmer</h3>${cf} You won't want to be walking any areas as that will only slow you down.`)+addChecked(bf);
+	return addToDescRow(`<h3>Cursed Farmer</h3>${cf} You won't want to be walking any areas as that will only slow you down.`)+addChecked(bf,true);
 }
 
 function pure2LL() {
 	var bf = 1053332137310143n;
-	return addToDescRow(`<h3>Roots of Loomlurch (100% 2j)</h3>${ll} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Roots of Loomlurch (100% 2j)</h3>${ll} ${bbb}`)+addChecked(bf,true);
 }
 
 function pure3LL() {
 	var bf = 1090715500149758n;
-	return addToDescRow(`<h3>Roots of Loomlurch (100% 3j)</h3>${ll} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Roots of Loomlurch (100% 3j)</h3>${ll} ${bbb}`)+addChecked(bf,true);
 }
 
 function pre4TT() {
 	var bf = 508470925670862n;
-	return addToDescRow(`<h3>Tall Tales (Mixed 3/4j)</h3>${tt} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (Mixed 3/4j)</h3>${tt} ${bbb}`)+addChecked(bf,true);
 }
 
 function pure4TT() {
 	var bf = 544309226487279n;
-	return addToDescRow(`<h3>Tall Tales (100% 4j)</h3>${tt} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 4j)</h3>${tt} ${bbb}`)+addChecked(bf,true);
 }
 
 function feat4TT() {
 	var bf = 544309226487279n;
-	return addToDescRow(`<h3>Tall Tales (100% 4j with Wasting Haste Feat)</h3>${tt} You'll need to equip Briv's Wasting Haste feat to return to pure 4j. ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 4j with Wasting Haste Feat)</h3>${tt} You'll need to equip Briv's Wasting Haste feat to return to pure 4j. ${bbb}`)+addChecked(bf,true);
 }
 
 function feat54TT() {
 	var bf = 498342527128532n;
-	return addToDescRow(`<h3>Tall Tales (100% 5j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 5j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf,true);
 }
 
 function pure6LL() {
 	var bf = 873863567932216n;
-	return addToDescRow(`<h3>Roots of Loomlurch (100% 6j)</h3>${ll} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Roots of Loomlurch (100% 6j)</h3>${ll} ${bbb}`)+addChecked(bf,true);
 }
 
 function feat64TT() {
 	var bf = 800122939009243n;
-	return addToDescRow(`<h3>Tall Tales (100% 6j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 6j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf,true);
 }
 
 function feat64RAC() {
 	var bf = 948417617686362n;
-	return addToDescRow(`<h3>Resolve Amongst Chaos (100% 6j / 4j Feat Swap)</h3>${rac} This does not benefit from any quest reduction blessings or perks.${fsa}${fcsa}`)+addChecked(bf);
+	return addToDescRow(`<h3>Resolve Amongst Chaos (100% 6j / 4j Feat Swap)</h3>${rac} This does not benefit from any quest reduction blessings or perks.${fsa}${fcsa}`)+addChecked(bf,true);
 }
 
 function pure6TT() {
 	var bf = 1086144474247034n;
-	return addToDescRow(`<h3>Tall Tales (100% 6j)</h3>${tt} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 6j)</h3>${tt} ${bbb}`)+addChecked(bf,true);
 }
 
 function pure7TT() {
 	var bf = 1108238796846077n;
-	return addToDescRow(`<h3>Tall Tales (100% 7j)</h3>${tt} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 7j)</h3>${tt} ${bbb}`)+addChecked(bf,true);
 }
 
 function feat74TT() {
 	var bf = 380222613385436n;
-	return addToDescRow(`<h3>Tall Tales (100% 7j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 7j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf,true);
 }
 
 function feat84TT() {
 	var bf = 57952963557919n;
-	return addToDescRow(`<h3>Tall Tales (100% 8j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 8j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}${fcsa}`)+addChecked(bf,true);
 }
 
 function pure8TT() {
 	var bf = 985128007367679n;
-	return addToDescRow(`<h3>Tall Tales (100% 8j)</h3>${tt} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 8j)</h3>${tt} ${bbb}`)+addChecked(bf,true);
 }
 
 function mixed89TT() {
 	var bf = 16989228054990n;
-	return addToDescRow(`<h3>Tall Tales (Mixed 8j/9j)</h3>${tt} ${bbb}<br />This becomes better than pure 8j (without feat swap) at around 65% 9 jump. Recommended to use Ezmerelda if you use Shandie to avoid loss of Dash.`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (Mixed 8j/9j)</h3>${tt} ${bbb}<br />This becomes better than pure 8j (without feat swap) at around 65% 9 jump. Recommended to use Ezmerelda if you use Shandie to avoid loss of Dash.`)+addChecked(bf,true);
 	comment+=addToDescRow(`&nbsp;`);
 }
 
 function feat94TT() {
 	var bf = 35181131988031n;
-	return addToDescRow(`<h3>Tall Tales (100% 9j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 9j / 4j Feat Swap)</h3>${tt} ${bbb}${fsa}`)+addChecked(bf,true);
 }
 
 function pure9TT() {
 	var bf = 17020252302587n;
-	return addToDescRow(`<h3>Tall Tales (100% 9j)</h3>${tt} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 9j)</h3>${tt} ${bbb}`)+addChecked(bf,true);
 }
 
 function pure11TT() {
 	var bf = 853162999544159n;
-	return addToDescRow(`<h3>Tall Tales (100% 11j)</h3>${tt} ${bbb}`)+addChecked(bf);
+	return addToDescRow(`<h3>Tall Tales (100% 11j)</h3>${tt} ${bbb}`)+addChecked(bf,true);
 }
 
 function addToDescRow(content) {
-	var ret=`<span class="routesRow"><span class="routesDesc">replace</span></span>`;
-	ret=ret.replace(`replace`,content);
-	return ret;
+	return `<span class="routesRow"><span class="routesDesc">${content}</span></span>`;
 }
 
 function determineRarity() {
 	switch(rarityInput.value) {
-		case "common":
+		case `common`:
 			return 0.1;
-		case "uncommon":
+		case `uncommon`:
 			return 0.3;
-		case "rare":
+		case `rare`:
 			return 0.5;
 		default:
 			return 1;
@@ -295,9 +399,9 @@ function determineRarity() {
 
 function determineGilding() {
 	switch(gildingInput.value) {
-		case "shiny":
+		case `shiny`:
 			return 1.5;
-		case "golden":
+		case `golden`:
 			return 2;
 		default:
 			return 1;
@@ -323,10 +427,12 @@ function mapFromToRange(v, oldMin, oldMax, newMin, newMax) {
 	return (v - oldMin) * ((newMax - newMin) / (oldMax - oldMin)) + newMin;
 }
 
-function addChecked(bf) {
-	var comment=addToDescRow(`&nbsp;`)+`<span class="routesRow">`;
+function addChecked(bf,br) {
+	var comment=``;
+	if (br) comment+=addToDescRow(`&nbsp;`);
+	comment+=`<span class="routesRow">`;
 	for (let i=1; i<=50; i++) {
-		var checked=isChecked(bf,i) ? " checked" : "";
+		var checked=isChecked(bf,i) ? ` checked` : ``;
 		comment+=`<span class="routesBoxes"><input type="checkbox" class="checkbox" id="z${i}" name="z${i}" ${checked} disabled><label class="cblabel" for="z${i}">${i}</label></span>`;
 	}
 	comment+=`</span>`;
@@ -346,4 +452,64 @@ function vSigFig(n,nDif) {
 		p++;
 	}
 	return p+1;
+}
+
+function swapTab() {
+	var hash = window.location.hash.substring(1);
+	if (hash != "" && document.getElementById(hash) != undefined) {
+		document.getElementById(hash).click();
+	}
+}
+
+function setHash(hash) {
+	hash = `#` + hash;
+	if(history.replaceState) {
+		history.replaceState(null, null, hash);
+	} else {
+		window.location.hash = hash;
+	}
+}
+
+function populateEventsList() {
+	var eventsList=document.getElementById(`eventsList`);
+	var contents=``;
+	for (let i=0;i<json.length;i++) {
+		var curr=json[i];
+		var onClick=` onClick="toggleEvent('${curr.name}','e${curr.id}')" style=""`;
+		var imgType=`Expand`;
+		if (curr.bitfields.length==0) {
+			onClick=` style="cursor:default;color:#777777;"`;
+			imgType=`Empty`;
+		}
+		contents+=addToDescRow(`<h2><a href="javascript:void(0);" id="${curr.name}"${onClick}>`+genExpColImg(imgType,curr.name)+` ${curr.name}</a></h2>`);
+		contents+=`<span class="routesRow"><span class="routesWrapper" id="e${curr.id}" hidden>`;
+		for (let k=0;k<curr.bitfields.length;k++) {
+			var bf = curr.bitfields[k];
+			if (k>0) contents+=addToDescRow(`&nbsp;`);
+			contents+=addToDescRow(`<h3>${curr.name}: ${bf.name}</h3>`);
+			if (bf.custom!=undefined&&bf.custom!="") {
+				contents+=addToDescRow(`${bf.custom}`);
+			} else {
+				contents+=addChecked(bf.bitfield,false);
+			}
+		}
+		contents+=`</span></span>`;
+	}
+	eventsList.innerHTML=contents;
+}
+
+function toggleEvent(name,id) {
+	var a=document.getElementById(name);
+	var ele=document.getElementById(id);
+	if (ele.hidden) {
+		a.innerHTML = genExpColImg("Collapse",name)+` ${name}`;
+		ele.hidden=false;
+	} else {
+		a.innerHTML = genExpColImg("Expand",name)+` ${name}`;
+		ele.hidden=true;
+	}
+}
+
+function genExpColImg(type,name) {
+	return `<img src="images/${type.toLowerCase()}.png" alt="${type} ${name} Icon">`;
 }
