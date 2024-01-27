f00e127a3d5841f03bfd8c76c1da6856c8721ebf
Emmote <emmote2@gmail.com>
2024-01-27 16:49:23 +0000

    Updating script.
    - This should allow people to share their routes by sharing the url hash.

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,4 @@
+const st=`stacksTab`;
 const ilvlInput=document.getElementById(`ilvl`);
 const presetsInput=document.getElementById(`presets`);
 const rarityInput=document.getElementById(`rarity`);
@@ -20,28 +21,29 @@ const stackMult=[1/((100-3.2)/100),1/((100-4)/100)];
 function init() {
 	populateEventChoices();
 	populateStackRoutes();
-	ilvlInput.addEventListener(`input`, update);
-	presetsInput.addEventListener(`change`, preset);
-	rarityInput.addEventListener(`change`, update);
-	gildingInput.addEventListener(`change`, update);
 	window.addEventListener('hashchange',() =>{swapTab();});
 	swapTab();
-	eventPresets.addEventListener(`change`, populateEventList);
-	eventChoices.addEventListener(`change`, populateEventList);
-	stackRoute.addEventListener(`change`, calculateStacks);
-	stackReset.addEventListener(`change`, calculateStacks);
-	stackFavour.addEventListener(`change`, calculateStacks);
-	stackStack.addEventListener(`change`, calculateStacks);
-	stackBrivZone.addEventListener(`change`, calculateStacks);
-	stackWithMetal.addEventListener(`change`, calculateStacks);
+	ilvlInput.addEventListener(`input`,update);
+	presetsInput.addEventListener(`change`,preset);
+	rarityInput.addEventListener(`change`,update);
+	gildingInput.addEventListener(`change`,update);
+	eventPresets.addEventListener(`change`,populateEventList);
+	eventChoices.addEventListener(`change`,populateEventList);
+	stackRoute.addEventListener(`change`,calculateStacks);
+	stackReset.addEventListener(`change`,calculateStacks);
+	stackFavour.addEventListener(`change`,calculateStacks);
+	stackStack.addEventListener(`change`,calculateStacks);
+	stackBrivZone.addEventListener(`change`,calculateStacks);
+	stackWithMetal.addEventListener(`change`,calculateStacks);
 	update();
+	calculateStacks();
 }
 
 function preset() {
 	rarityInput.value=`epic`;
 	gildingInput.value=`golden`;
 	
-	var jumps=presetsInput.value.replace(/[^\d]/g, '');
+	let jumps=presetsInput.value.replace(/[^\d]/g,'');
 	ilvlInput.value=500*Math.pow(2,jumps-1)-374;
 	update();
 }
@@ -56,7 +58,7 @@ function update() {
 		}
 	} else {
 		if (gildingInput.options.length==2) {
-			var opt=document.createElement(`option`);
+			let opt=document.createElement(`option`);
 			opt.text=`Golden`;
 			opt.value=`golden`;
 			gildingInput.options.add(opt);
@@ -67,29 +69,29 @@ function update() {
 	} else if (gildingInput.value!=`shiny`&&shinyNote.style.display=='') {
 		shinyNote.style.display='none';
 	}
-	var ilvls=ilvlInput.value;
+	let ilvls=ilvlInput.value;
 	if (ilvls<1) {
 		document.getElementById(`ilvl`).value=1;
 		ilvls=1;
 	}
-	var rarity=determineRarity();
-	var gilding=determineGilding();
-	var skips=determineJumps(ilvls,rarity,gilding);
-	var p=(skips[1]*100);
-	var np=((1-skips[1])*100);
-	var jumps=(skips[0]-1)+skips[1];
+	let rarity=determineRarity();
+	let gilding=determineGilding();
+	let skips=determineJumps(ilvls,rarity,gilding);
+	let p=(skips[1]*100);
+	let np=((1-skips[1])*100);
+	let jumps=(skips[0]-1)+skips[1];
 	
-	var skipsL=determineJumps(ilvls-1,rarity,gilding);
-	var pL=(skipsL[1]*100);
-	var pDiff=p-pL;
+	let skipsL=determineJumps(ilvls-1,rarity,gilding);
+	let pL=(skipsL[1]*100);
+	let pDiff=p-pL;
 	
-	var fix=vSigFig(p,pDiff);
+	let fix=vSigFig(p,pDiff);
 	p=p.toFixed(fix);
 	np=np.toFixed(fix);
 	
-	var comment=``;
-	var spacer=addToDescRow(`&nbsp;`)
-	var skipBlurb=`${p}% chance to skip ${skips[0]} areas.`;
+	let comment=``;
+	let spacer=addToDescRow(`&nbsp;`)
+	let skipBlurb=`${p}% chance to skip ${skips[0]} areas.`;
 	if (np>0) {
 		skipBlurb+=`<br>${np}% chance to skip ${skips[0]-1} areas.`;
 	}
@@ -162,53 +164,45 @@ function addToDescRow(content) {
 
 function determineRarity() {
 	switch(rarityInput.value) {
-		case `common`:
-			return 0.1;
-		case `uncommon`:
-			return 0.3;
-		case `rare`:
-			return 0.5;
-		default:
-			return 1;
+		case `common`:return 0.1;
+		case `uncommon`:return 0.3;
+		case `rare`:return 0.5;
+		default:return 1;
 	}
 }
 
 function determineGilding() {
 	switch(gildingInput.value) {
-		case `shiny`:
-			return 1.5;
-		case `golden`:
-			return 2;
-		default:
-			return 1;
+		case `shiny`:return 1.5;
+		case `golden`:return 2;
+		default:return 1;
 	}
 }
 
 function determineJumps(ilvls,rarity,gilding) {
-	let effect=0.25 * (1+(0.004*gilding*ilvls*rarity + 0.996*gilding*rarity));
-	
-	let skips=1
-	let skipChance=effect
+	let effect=0.25*(1+(0.004*gilding*ilvls*rarity+0.996*gilding*rarity));
+	let skips=1;
+	let skipChance=effect;
 	if (effect>1) {
 		while (skipChance>1) {
-			skipChance /= 2
-			skips+=1
+			skipChance/=2;
+			skips+=1;
 		}
-		skipChance=mapFromToRange(skipChance, 0.5, 1.0, 0.01, 1)
+		skipChance=mapFromToRange(skipChance,0.5,1.0,0.01,1);
 	}
 	return [skips,skipChance];
 }
 
-function mapFromToRange(v, oldMin, oldMax, newMin, newMax) {
-	return (v - oldMin) * ((newMax - newMin) / (oldMax - oldMin)) + newMin;
+function mapFromToRange(v,oldMin,oldMax,newMin,newMax) {
+	return (v-oldMin)*((newMax-newMin)/(oldMax-oldMin))+newMin;
 }
 
 function addChecked(bf,br) {
-	var comment=``;
+	let comment=``;
 	if (br) comment+=addToDescRow(`&nbsp;`);
 	comment+=`<span class="routesRow">`;
-	for (let i=1; i<=50; i++) {
-		var checked=isChecked(bf,i) ? ` checked` : ``;
+	for (let i=1;i<=50;i++) {
+		let checked=isChecked(bf,i)?` checked`:``;
 		comment+=`<span class="routesBoxes"><input type="checkbox" class="checkbox" id="z${i}" name="z${i}" ${checked} disabled><label class="cblabel" for="z${i}">${i}</label></span>`;
 	}
 	comment+=`</span>`;
@@ -216,46 +210,46 @@ function addChecked(bf,br) {
 }
 
 function addLoop(bf,j,fs) {
-	let s = j+1;
-	let w = fs ? 5 : 1;
-	let z = 1;
-	let route = [z];
-	while (z <= 200) {
-		z += isChecked(bf,z%50||50) ? s : w;
+	let s=j+1;
+	let w=fs?5:1;
+	let z=1;
+	let route=[z];
+	while (z<=200) {
+		z+=isChecked(bf,z%50||50)?s:w;
 		route.push(z);
 	}
-	let lastOfLoop = route[route.length-1] % 50;
-	let loopStartIndex = 0;
-	for (let i=route.length-2; i>=0; i--) {
-		if (route[i] % 50 == lastOfLoop) {
-			loopStartIndex = i;
+	let lastOfLoop=route[route.length-1]%50;
+	let loopStartIndex=0;
+	for (let i=route.length-2;i>=0;i--) {
+		if (route[i]%50==lastOfLoop) {
+			loopStartIndex=i;
 			break;
 		}
 	}
-	let loop = [];
-	for (let i=loopStartIndex; i<route.length-1; i++) {
-		loop.push(route[i] % 50 || 50);
+	let loop=[];
+	for (let i=loopStartIndex;i<route.length-1;i++) {
+		loop.push(route[i]%50||50);
 	}
 	if (loop[0]==50) {
-		loop.splice(0, 1);
+		loop.splice(0,1);
 		loop.push(50);
 	}
-	let spacer = ` - `;
-	var comment=`<br>Preferred Loop: `;
-	for (let i=0; i<loop.length; i++) {
-		if (i > 0) comment += spacer;
-		comment += loop[i];
+	let spacer=` - `;
+	let comment=`<br>Preferred Loop: `;
+	for (let i=0;i<loop.length;i++) {
+		if (i > 0) comment+=spacer;
+		comment+=loop[i];
 	}
-	comment += `${spacer} repeat`;
+	comment+=`${spacer} repeat`;
 	return comment;
 }
 
 function isChecked(bf,i) {
-	return (bf & BigInt(Math.pow(2,i-1))) != 0 ? true : false;
+	return (bf & BigInt(Math.pow(2,i-1)))!=0?true:false;
 }
 
 function vSigFig(n,nDif) {
-	var p = 2;
+	let p=2;
 	if (n==100) {
 		return p;
 	}
@@ -266,25 +260,40 @@ function vSigFig(n,nDif) {
 }
 
 function swapTab() {
-	var hash = window.location.hash.substring(1);
-	if (hash != "" && document.getElementById(hash) != undefined) {
-		document.getElementById(hash).click();
+	let hash=window.location.hash.substring(1).split("_");
+	if (hash[0]!=""&&document.getElementById(hash[0])!=undefined) {
+		document.getElementById(hash[0]).click();
+	}
+	if (hash.length>1) {
+		switch (hash[0]) {
+			case `${st}`:
+				stackRoute.value=hash[1];
+				stackReset.value=hash[2];
+				stackFavour.value=hash[3];
+				stackStack.value=hash[4];
+				stackBrivZone.value=hash[5];
+				stackWithMetal.checked=hash[6]==`1`?true:false;
+				break;
+		}
 	}
 }
 
 function setHash(hash) {
-	hash = `#` + hash;
+	if (hash==st) {
+		hash=`${st}_${stackRoute.value}_${stackReset.value}_${stackFavour.value}_${stackStack.value}_${stackBrivZone.value}_`+(stackWithMetal.checked?1:0);
+	}
+	hash=`#`+hash;
 	if(history.replaceState) {
-		history.replaceState(null, null, hash);
+		history.replaceState(null,null,hash);
 	} else {
-		window.location.hash = hash;
+		window.location.hash=hash;
 	}
 }
 
 function populateEventChoices() {
 	for (let i=0;i<eventsJson.length;i++) {
 		if (eventsJson[i].bitfields.length==0) continue;
-		var opt=document.createElement("option");
+		let opt=document.createElement("option");
 		opt.value=eventsJson[i].id;
 		opt.text=eventsJson[i].name;
 		eventChoices.add(opt);
@@ -292,11 +301,11 @@ function populateEventChoices() {
 }
 
 function populateEventList() {
-	var jumps=eventPresets.value;
-	var event=eventChoices.value;
+	let jumps=eventPresets.value;
+	let event=eventChoices.value;
 	if (event==``) return;
 	
-	var curr=null;
+	let curr=null;
 	for (let i=0;i<eventsJson.length;i++) {
 		if (eventsJson[i].id==event) {
 			curr=eventsJson[i];
@@ -305,9 +314,9 @@ function populateEventList() {
 	}
 	if (curr==null) return;
 	
-	var contents=``;
+	let contents=``;
 	for (let k=0;k<curr.bitfields.length;k++) {
-		var bf = curr.bitfields[k];
+		let bf=curr.bitfields[k];
 		if (jumps==`all`||jumps==bf.name) {
 			if (k>0&&jumps==`all`) contents+=addToDescRow(`&nbsp;`);
 			contents+=addToDescRow(`<h3>${curr.name}: ${bf.name}</h3>`);
@@ -326,31 +335,33 @@ function populateStackRoutes() {
 	let keyset=Object.keys(gemFarmJson);
 	for (let key of keyset) {
 		if (typeof(gemFarmJson[key].jump)!="number") continue;
-		var opt=document.createElement("option");
+		let opt=document.createElement("option");
 		opt.value=key;
 		opt.text=parseName(gemFarmJson[key],true);
 		stackRoute.add(opt);
 	}
 	stackRoute.value=`pure4TT`;
-	calculateStacks();
 }
 
 function calculateStacks() {
-	var contents=``;
+	let contents=``;
 	if (stackRoute.value==""||stackFavour.value==""||stackReset.value==""||stackBrivZone.value=="") {
 		contents+=addToDescRow(`Need more data.`);
 		contents+=addToDescRow(`&nbsp;`);
 		stackResult.innerHTML=contents;
 		return
 	}
+	if (window.location.hash.substring(1).split("_")[0]==st) {
+		setHash(st);
+	}
 	let f=Number(stackFavour.value);
 	let r=Number(stackReset.value);
 	let bz=Number(stackBrivZone.value);
 	let t=Math.min(f,Math.floor(r/5))+1;
 	let jsonRoute=gemFarmJson[stackRoute.value];
-	let s = Number(jsonRoute.jump)+1;
-	let w = jsonRoute.fs||false?5:1;
-	let z = t;
+	let s=Number(jsonRoute.jump)+1;
+	let w=jsonRoute.fs||false?5:1;
+	let z=t;
 	let mj=0;
 	let nmj=0;
 	let stacks=50;
@@ -358,7 +369,7 @@ function calculateStacks() {
 	while (z<=r&&route.length<2000) {
 		let checked=z>=bz&&isChecked(jsonRoute.bf,z%50||50);
 		let metal=!(!stackWithMetal.checked&&z<stackStack.value);
-		z += checked ? s : w;
+		z+=checked?s:w;
 		route.push(z);
 		if ((checked||w>1)&&z<r&&z>=bz) {
 			stacks=Math.ceil((stacks-0.5)*(metal?stackMult[0]:stackMult[1]));
